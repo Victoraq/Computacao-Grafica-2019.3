502f26738d416d957c910b99d748a9be25bb2af3
Victor Aquiles <victoraqalencar@gmail.com>
2019-09-09 22:10:33 -0300

    Trabalho 01

---
+++ Trabalho01/main.cpp
@@ -0,0 +1,204 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <cstdlib>
+#include <time.h>
+#include <math.h>
+
+#include "extras.h"
+
+/// Estruturas iniciais para armazenar vertices
+//  Você poderá utilizá-las adicionando novos métodos (de acesso por exemplo) ou usar suas próprias estruturas.
+
+/// Globals
+float zdist = 5.0;
+float rotationX = 0.0, rotationY = 0.0;
+int   last_x, last_y;
+int   width, height;
+int proj = 1;                      // Indica em que projeção será exibido
+
+
+/// Functions
+void init(void)
+{
+    initLight(width, height); // Função extra para tratar iluminação.
+}
+
+
+void drawCampo(void) {
+
+    // Paredes
+    glPushMatrix();
+        setColor(0.765, 0.796, 0.851);
+        glTranslatef(-3,2.0,0.5);
+        glScalef(1,25.0, 1.0);
+        glutSolidCube(0.25);
+    glPopMatrix();
+
+    glPushMatrix();
+        setColor(0.765, 0.796, 0.851);
+        glTranslatef(3,2.0,0.5);
+        glScalef(1,25.0, 1.0);
+        glutSolidCube(0.25);
+    glPopMatrix();
+
+    glPushMatrix();
+        setColor(0.765, 0.796, 0.851);
+        glTranslatef(0,-1.25,0.5);
+        glScalef(25,1.0, 1.0);
+        glutSolidCube(0.25);
+    glPopMatrix();
+
+    glPushMatrix();
+        setColor(0.765, 0.796, 0.851);
+        glTranslatef(0,5.25,0.5);
+        glScalef(25,1.0, 1.0);
+        glutSolidCube(0.25);
+    glPopMatrix();
+}
+
+void display(void)
+{
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /* Mudando projeção */
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+
+    // Mudança entre visão perspectiva e ortogonal
+    int h = height;
+    int w = width;
+    int ortho = 5;
+    int proj_vision; // muda o y para ajustar a camera durante a mudança de perspectiva
+
+    if(proj == -1) { // Visão ortogonal
+        if (width <= height)
+            glOrtho (-ortho, ortho, -ortho*h/w, ortho*h/w, -100.0, 100.0);
+        else
+            glOrtho (-ortho*w/h, ortho*w/h, -ortho, ortho, -100.0, 100.0);
+        proj_vision = 3.5;
+    }
+    else { // Visão perspectiva
+        gluPerspective(60.0f,(GLfloat)width/(GLfloat)height,0.01f,200.0f);	// Calculate The Aspect Ratio Of The Window
+        proj_vision = -3.5;
+    }
+
+    /* Camera e desenhos */
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    gluLookAt (0.0, proj_vision, zdist, 0.0, 3.0, 0.0, 0.0, 1.0, 0.0);
+
+    glPushMatrix();
+        glRotatef( rotationY, 0.0, 1.0, 0.0 );
+        glRotatef( rotationX, 1.0, 0.0, 0.0 );
+
+        setMaterials();
+        drawCampo(); // Campo
+
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+void idle ()
+{
+    float t, desiredFrameTime, frameTime;
+    static float tLast = 0.0;
+
+    // Get elapsed time
+    t = glutGet(GLUT_ELAPSED_TIME);
+    // convert milliseconds to seconds
+    t /= 1000.0;
+
+    // Calculate frame time
+    frameTime = t - tLast;
+    // Calculate desired frame time
+    desiredFrameTime = 1.0 / (float) (60);
+
+    // Check if the desired frame time was achieved. If not, skip animation.
+    if(frameTime <= desiredFrameTime)
+        return;
+
+    // **  UPDATE ANIMATION VARIABLES ** //
+    float step = 100; // Speed of the animation
+
+    // Update tLast for next time, using static local variable
+    tLast = t;
+    glutPostRedisplay();
+}
+
+void reshape (int w, int h)
+{
+    width = w;
+    height = h;
+
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+
+}
+
+void keyboard (unsigned char key, int x, int y)
+{
+
+    switch (tolower(key))
+    {
+        case 'p': // Mudança de perspectiva
+            proj *= -1;
+            break;
+        case 27:
+            exit(0);
+            break;
+    }
+}
+
+// Motion callback
+void motion(int x, int y )
+{
+    rotationX += (float) (y - last_y);
+    rotationY += (float) (x - last_x);
+
+    last_x = x;
+    last_y = y;
+}
+
+// Mouse callback
+void mouse(int button, int state, int x, int y)
+{
+    if ( button == GLUT_LEFT_BUTTON && state == GLUT_DOWN )
+    {
+        last_x = x;
+        last_y = y;
+    }
+    if(button == 3) // Scroll up
+    {
+        zdist+=1.0f;
+    }
+    if(button == 4) // Scroll Down
+    {
+        zdist-=1.0f;
+    }
+}
+
+/// Main
+int main(int argc, char** argv)
+{
+    // Mudando random seed
+    srand(time(NULL));
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize (1000, 600);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow (argv[0]);
+    init ();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMouseFunc( mouse );
+    glutMotionFunc( motion );
+    glutKeyboardFunc(keyboard);
+    glutIdleFunc(idle);
+    glutMainLoop();
+    return 0;
+}
